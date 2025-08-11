eb5b7a851edcd7e1c62acb6827bb088f4a6779d5
Anthony <mitsuaky2015@outlook.es>
2025-08-11 17:39:30 -0500

    primercv

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import svgr from '@svgr/rollup';
+
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+// https://vite.dev/config/
+export default defineConfig({
+  base: isGitHubPages ? '/portafolio/' : './',
+  plugins: [react(), svgr()],
+})
